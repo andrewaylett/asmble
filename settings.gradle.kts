a66e1a5646d714601ecf26925b509838a44c1b57
Andrew Aylett <andrew@aylett.co.uk>
2022-04-03 18:37:03 +0100

    Always submit build scan from CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,18 @@ rootProject.name = "asmble"
 include("annotations")
 include("compiler")
 include("cli")
+
+plugins {
+    id("com.gradle.enterprise") version ("3.9")
+}
+
+gradleEnterprise {
+    buildScan {
+        publishAlways()
+
+        if (System.getProperty("enterprise.tos.agree", "no") == "yes") {
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
